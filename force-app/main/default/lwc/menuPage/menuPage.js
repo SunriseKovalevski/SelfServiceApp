6c3759effb6c8ab13ef2eb36a75686f01d137bc1
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2020-10-08 18:29:40 +0300

    feat: add js for menu

--- force-app/main/default/lwc/menuPage/menuPage.js
+++ force-app/main/default/lwc/menuPage/menuPage.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getMenuItems from '@salesforce/apex/ItemController.getMenuItems';
+import getGroups from '@salesforce/apex/ItemController.getGroups';
+import getMainGroups from '@salesforce/apex/ItemController.getMainGroups';
+import getItemsTree from '@salesforce/apex/ItemController.getItemsTree';
 
-export default class MenuPage extends LightningElement {}
+const columns = [
+    { label: 'Название', fieldName: 'Name' },
+    { label: 'Цена', fieldName: 'Cost__c', type: 'currency' },
+    { label: 'Описание блюда', fieldName: 'Description__c' },
+    { label: 'Количество порций', fieldName: 'Portions__c' },
+    { label: 'Комментарий', fieldName: 'Comment__c' },
+];
+
+
+
+
+export default class MenuPage extends LightningElement {
+    error;
+    columns = columns;
+
+
+    @wire(getMenuItems) menuItems;
+   //  @wire(getGroups) groupItems;
+  //   @wire(getMainGroups) mainGroupItems;
+   @wire(getItemsTree) itemsTree;
+    
+   
+}
+
